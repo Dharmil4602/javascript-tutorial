5703de2cb6a0a9777585037bcb9a69ffa447e6a7
Dharmil Shah <dharmilshah476@gmail.com>
2021-07-30 01:03:29 +0530

    Math - Complete JS Tutorial

---
+++ Complete-JS/math.js
@@ -0,0 +1,15 @@
+let z;
+z = Math.PI;
+z = Math.E;
+z = Math.round(3.2); // Round offs the value
+z = Math.ceil(6.3); // Upper value is printed
+z = Math.floor(3.8); // lower value is printed
+z = Math.abs(-3.8); // abs prints only positive number, no matter what the input is
+z = Math.sqrt(9); // prints the square root of given number
+z = Math.pow(2, 3); // (number, power)
+z = Math.max(2000, 50, 100, 6, 9); // prints maximum number out of given numbers
+z = Math.min(2, 3, 54, 200, 3000); // prints minimum number out of given numbers
+z = Math.random(); // By default random number is generated between 0 to 1
+z = 100000 + (1000000 - 100000)*Math.random(); // Generating random number from particular number to particular number
+z = Math.round(100000 + (1000000 - 100000)*Math.random()); // Generating random number like 6 digit otp
+console.log(z);
