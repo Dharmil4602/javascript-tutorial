67b13bbcc94ba63d637e6ed96a75d8885e0d1914
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-10 01:27:43 +0530

    Just Started The Library Websie

---
+++ Complete-JS/library.js
@@ -0,0 +1,22 @@
+console.log(`Hello Ji`);
+
+class Library
+{
+
+    constructor(bookList)
+    {
+
+    }
+    
+getBookList()
+{
+
+}
+
+issueBook(bookname, user)
+{
+
+}
+
+returnBook(bookname)
+}
