847fea93a809027f22fc9bb6138cca1a28d083fe
Dharmil Shah <dharmilshah476@gmail.com>
2021-09-12 01:01:43 +0530

    Arrow Function

---
+++ Complete-JS/arrowfunc.js
@@ -0,0 +1,7 @@
+const greet = () => `Good Morning Dharmil`;
+console.log(greet());
+
+// Using Arrow Function For Object.
+
+const greet1 = () => ({name: `Dharmil`}); // In one liner we need to include object inside Ciruclar braces.
+console.log(greet1());
